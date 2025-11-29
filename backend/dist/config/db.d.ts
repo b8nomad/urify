18dbcc6ce5b914c9546e88413d518dcbe7a380e8
b8nomad <b8nomad@gmail.com>
2025-11-30 02:03:29 +0530

    fix structure

--- backend/dist/config/db.d.ts
+++
@@ -1,3 +0,0 @@
-import { type Connection } from "mongoose";
-export default function connectDB(): Promise<Connection>;
-//# sourceMappingURL=db.d.ts.map
--- backend/dist/routes/slug.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import slugController from "../controller/slug.controller.js";
-const router = Router();
-router.post("/", slugController.createSlug);
-router.get("/", slugController.getSlug);
-router.put("/", slugController.editSlug);
-export default router;
-//# sourceMappingURL=slug.route.js.map
